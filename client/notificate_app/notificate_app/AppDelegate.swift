e4f2c37a2b22cd3827b97f7bd6d101f4c990f34b
Matama091 <matama@pluslab.org>
2023-01-06 12:30:56 +0900

    [Add]push:custom data

--- client/notificate_app/notificate_app/AppDelegate.swift
+++ client/notificate_app/notificate_app/AppDelegate.swift
@@ -51,4 +51,36 @@ extension AppDelegate {
                      didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("Failed to register to APNs: \(error)")
     }
+    
+    
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // ユーザー通知の許可がなくても、Remote Notificationを受信したらここが実行される。
+        // Remote Notificationのペイロードに `"content-available": 1` が入っていると、バックグラウンドからここが呼ばれる
+
+        // UserNotificationCenterDelegateも実装していた場合、
+        // アプリがフォアグラウンドで実行中は、
+        // このメソッドと合わせて、`userNotificationCenter(_:willPresent:withCompletionHandler:)`も呼ばれる。
+        // → 処理が重複しないように注意する必要がある
+        // → バックグラウンド判定には、UIApplication.shared.applicationState == .background が使えそう
+
+        // ここで処理する内容は、
+        // userInfoからカスタムペイロードを取り出したり……
+//        let isNewDataAvailable = userInfo["new-data-available"] as! Bool
+        // UserDefaultsにセットして、次回の起動時にフェッチさせるようにしたり……
+//        UserDefaults.standard.set(isNewDataAvailable, forKey: "new-data-available")
+        // サーバと同期したり
+//        if isNewDataAvailable {
+//            let remote = MyServiceRepository()  // あくまでサンプル
+//            let newData = remote.fetch()  // 非同期処理しているとする
+//            MyLocalStorage().setApplicationData(newData)
+//        }
+        let path_id = userInfo["path_id"] as? String
+        textContent = path_id ?? ""
+        NSLog("text: \(String(describing: path_id))")
+
+        // そして30秒以内に、completionHandlerを呼ぶ（必須）
+        completionHandler(.newData)
+    }
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -8,17 +9,41 @@ import (
 	"github.com/sideshow/apns2/certificate"
 )
 
+type Payload struct {
+	APS    APS    `json:"aps"`
+	PathID string `json:"path_id"`
+}
+
+type APS struct {
+	Alert            string `json:"alert"`
+	ContentAvailable int    `json:"content-available"`
+}
+
 func main() {
 
 	cert, err := certificate.FromP12File("./notification_cert.p12", "")
 	if err != nil {
 		log.Fatal("Cert Error:", err)
 	}
+	path_id := "12345678"
+
+	payload := Payload{
+		APS: APS{
+			Alert:            "hello pluslab's member",
+			ContentAvailable: 1,
+		},
+		PathID: path_id,
+	}
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal("Json Error:", err)
+	}
 
 	notification := &apns2.Notification{
 		DeviceToken: "308acbe1ef90d4872861598aab223057d1099e4112f1508380655b9d0dd19cd5",
 		Topic:       "org.pluslab.notification",
-		Payload:     []byte(`{"aps":{"alert":"hello pluslab's member"}}`),
+		Payload:     []byte(data),
 	}
 
 	// select Develop or Production
